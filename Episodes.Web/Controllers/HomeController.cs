379a82a632705fb8a11fffb8acc1d396f0c6e6b3
ionoy <mihhail.maslakov@gmail.com>
2013-02-08 13:34:15 +0200

    initial commit

---
+++ Episodes.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Episodes.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
